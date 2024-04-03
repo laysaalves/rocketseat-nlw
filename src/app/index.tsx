45cceb1a898bc49ad31f20314961f73bb883d4b3
Laysa <laysa.developer@gmail.com>
2024-04-03 18:08:24 -0300

    feat: statusBarHeight add

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
-import { View, Image, StatusBar } from "react-native";
+import { useState } from "react";
+import { View, Image, StatusBar, Alert } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 import { Link } from "expo-router"
 
@@ -7,7 +8,17 @@ import { colors } from "@/styles/colors"
 import { Input } from "@/components/input"
 import { Button } from "@/components/button"
 
+const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22:64;
+
 export default function Home() {
+  const [code, setCode] = useState("")
+
+  function handleAccessCredential(){
+    if(!code.trim()){
+      return Alert.alert("Ingresso", "Informe o código do ingresso!")
+    }
+  }
+
  return (
    <View className="flex-1 bg-green-500 items-center justify-center p-8">
     <StatusBar barStyle="light-content"/>
@@ -16,15 +27,20 @@ export default function Home() {
     className="h-16"
     resizeMode="contain"
     />
-    <View className="w-full mt-12 gap-3">
+   <View className="w-full mt-12 gap-3">
       <Input>
       <MaterialCommunityIcons name="ticket-confirmation-outline"
       color={colors.green[200]}
       size={20}
       />
-        <Input.Field placeholder="Código do ingresso" />
+      <Input.Field 
+      placeholder="Código do ingresso" 
+      onChangeText={setCode}
+      />
       </Input>
-      <Button title="Acessar credencial" />
+      <Button title="Acessar credencial" 
+      onPress={handleAccessCredential}
+      />
 
       <Link 
       href="/register" 
